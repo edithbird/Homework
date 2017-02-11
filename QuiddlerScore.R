eb66d1558702bb939501a3ccf4f19289b9d116fb
edithbird <edithbird5@gmail.com>
2017-02-11 15:54:08 -0500

    Quiddler Scoreboard Saturday 2/11

---
+++ QuiddlerScore.R
@@ -0,0 +1,20 @@
+r1 <- 19
+r2 <- 15
+r3 <- 30
+r4 <- 30
+r5 <- 38
+r6 <- 43
+r7 <- 34
+r8 <- 51
+christototal <- sum(r1, r2, r3, r4, r5, r6, r7, r8)
+pr1 <- 16
+pr2 <- 14
+pr3 <- 17
+pr4 <- 31
+pr5 <- 37
+pr6 <- 31
+pr7 <- 39
+pr8 <- 50
+paultototal <- sum(pr1, pr2, pr3,pr4, pr5, pr6, pr7, pr8)
+christototal
+paultototal
