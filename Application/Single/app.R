e234a0c93bee594129cb2f31aebd0300ee8c0d47
edithbird <edithbird5@gmail.com>
2017-02-27 11:17:51 -0500

    Full app working. Bar graphs working with color.

--- Application/Single/app.R
+++ Application/Single/app.R
@@ -67,8 +67,8 @@ mainPanel(
   
 
       tableOutput("table1"),
-      plotOutput("barplot")
-      #plotOutput("barplot2")
+      plotOutput("barplot"),
+      plotOutput("barplot2")
 
     )
   ))
@@ -87,6 +87,7 @@ server <- function(input, output) ({
     
     output$barplot <- renderPlot({
       
+      cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
       Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
       ScorePl1 <- c(input$integer1,input$integer3, input$integer5,input$integer7,input$integer9, input$integer11,input$integer13,input$integer15)
       ScorePl2 <- c(input$integer2,input$integer4, input$integer6,input$integer8,input$integer10, input$integer12,input$integer14,input$integer16)
@@ -94,8 +95,10 @@ server <- function(input, output) ({
       Data <- data.frame(Round, ScorePl1, ScorePl2)
       
       
-      ggplot(Data, aes(x=Round, y=ScorePl1)) + geom_bar(stat = "identity") + 
-        labs(x="Round", y="Score")
+      ggplot(Data, aes(x=Round, y=ScorePl1)) + geom_bar(stat = "identity", fill ="blue") + 
+        labs(x="Round", y="Score") +
+        scale_fill_hue(c=45, l=80) + ggtitle("Player 1") + 
+        theme(plot.title = element_text(lineheight= 3.8, face="bold"))
       
       
       
@@ -103,6 +106,21 @@ server <- function(input, output) ({
     
       
     
+    })
+    
+    output$barplot2 <- renderPlot({
+      cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+      Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
+      ScorePl1 <- c(input$integer1,input$integer3, input$integer5,input$integer7,input$integer9, input$integer11,input$integer13,input$integer15)
+      ScorePl2 <- c(input$integer2,input$integer4, input$integer6,input$integer8,input$integer10, input$integer12,input$integer14,input$integer16)
+      
+      Data <- data.frame(Round, ScorePl1, ScorePl2)
+      
+      
+      ggplot(Data, aes(x = Round, y = ScorePl2)) + geom_bar(stat = "identity", fill = "pink") + 
+        labs(x="Round", y="Score") +
+        scale_fill_hue(c=45, l=80) + ggtitle("Player 2") + 
+        theme(plot.title = element_text(lineheight= 3.8, face="bold"))
     })
  })
     
