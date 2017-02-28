4893e45e5b68c7e71b67479546624db424135bc5
edithbird <edithbird5@gmail.com>
2017-02-27 22:40:25 -0500

    Cleaned up some of the unnecessary text

--- ThisIsTheOne/app.R
+++ ThisIsTheOne/app.R
@@ -4,12 +4,11 @@ library(ggplot2)
 
 ui <- fluidPage(
   
-  # Application title
-  h1("Quiddler Score"#, align = "center"
-     ),
+
+  h1("Quiddler Score"),
   mainPanel(
     fluidRow(
-      column(4, #offset = 2,
+      column(4, 
              h2("Player 1"), 
              numericInput("integer1", "Round 1:",
                           min=-15, max=60, value=0),
@@ -57,7 +56,7 @@ ui <- fluidPage(
                           min=-15, max=60, value=0)
       ),
       
-      column(4, #offset = 2, 
+      column(4, 
              h2("Player 3"), 
              numericInput("integer17", "Round 1:", 
                           min=-15, max=60, value=0),
@@ -86,13 +85,13 @@ ui <- fluidPage(
       
       
       column(12, offset = 4,
-             h2("Scores"#, align = "center"
+             h2("Scores"
                 ), 
       tableOutput("table1")),
-      column(4, #offset = 2,
+      column(4,
              h2("Player 1"),
       plotOutput("barplot")),
-      column(4, #offset = 2,
+      column(4, 
              h2("Player 2"),
       plotOutput("barplot2")),
       column(4,
@@ -103,8 +102,6 @@ ui <- fluidPage(
     
   ))
 
-
-# Define server logic required to draw a histogram
 server <- function(input, output) ({
   
   output$table1 <- renderTable({
@@ -117,8 +114,9 @@ server <- function(input, output) ({
     )})
   
   output$barplot <- renderPlot({
+    #rainbowcols <- rainbow(8)
+    color <- c("slateblue3", "yellow", "red","slateblue3", "yellow", "red","slateblue3","red")
     
-    cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
     Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
     ScorePl1 <- c(input$integer1,input$integer3, input$integer5,input$integer7,input$integer9, input$integer11,input$integer13,input$integer15)
     ScorePl2 <- c(input$integer2,input$integer4, input$integer6,input$integer8,input$integer10, input$integer12,input$integer14,input$integer16)
@@ -126,26 +124,16 @@ server <- function(input, output) ({
     Data <- data.frame(Round, ScorePl1, ScorePl2)
     
     
-    ggplot(Data, aes(x=Round, y=ScorePl1)) + geom_bar(stat = "identity",  size = 2, fill ="slateblue3") + 
+    ggplot(Data, aes(x=Round, y=ScorePl1)) + geom_bar(stat = "identity",  size = 2, fill = color) + #"slateblue3"
       labs(x="Round", y="Score") +
       scale_fill_hue(c=45, l=80) +  #labs(title = "Player 1 Score") + 
-      ylim(-15, 80) + theme(panel.grid.major = element_blank(),
-                                                                                           panel.grid.minor = element_blank(),
-                                                                                           panel.border = element_blank(),
-                                                                                           panel.background = element_blank())
-      #ggtitle("Player 1") + 
-      #theme(plot.title = element_text(lineheight= 3.8, face="bold") + theme(plot.title = element_text(size = rel(2))))
-    
-    
-    
-    
-    
-    
-    
-  })
+      ylim(-15, 80) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), panel.background = element_blank())
+      
+     }) 
   
   output$barplot2 <- renderPlot({
-    cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    color <- c("slateblue3", "yellow", "red","slateblue3", "yellow", "red","slateblue3","red")
+    rainbowcols <- rainbow(8)
     Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
     ScorePl1 <- c(input$integer1,input$integer3, input$integer5,input$integer7,input$integer9, input$integer11,input$integer13,input$integer15)
     ScorePl2 <- c(input$integer2,input$integer4, input$integer6,input$integer8,input$integer10, input$integer12,input$integer14,input$integer16)
@@ -153,44 +141,32 @@ server <- function(input, output) ({
     Data <- data.frame(Round, ScorePl1, ScorePl2, ScorePl3)
     
     
-    ggplot(Data, aes(x = Round, y = ScorePl2)) + geom_bar(stat = "identity", size = 2, fill = "yellow2") + 
+    ggplot(Data, aes(x = Round, y = ScorePl2)) + geom_bar(stat = "identity", size = 2, fill = color) + #"yellow
       labs(x="Round", y="Score") +
-      scale_fill_hue(c=45, l=80) + #labs(title = "Player 2 Score") + 
+      scale_fill_hue(c=45, l=80) +
       ylim(-15, 80) + theme(panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.border = element_blank(),
     panel.background = element_blank())
-      #ggtitle("Player 3") + 
-      #theme(plot.title = element_text(lineheight= 3.8, face="bold")) + theme(plot.title = element_text(size = rel(2)))
   })
   
   
   output$barplot3 <- renderPlot({
-    cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    color <- c("slateblue3", "yellow", "red","slateblue3", "yellow", "red","slateblue3","red")
+    rainbowcols <- rainbow(8)
     Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
-    #ScorePl1 <- c(input$integer1,input$integer3, input$integer5,input$integer7,input$integer9, input$integer11,input$integer13,input$integer15)
-    #ScorePl2 <- c(input$integer2,input$integer4, input$integer6,input$integer8,input$integer10, input$integer12,input$integer14,input$integer16)
     ScorePl3 <- c(input$integer17,input$integer18, input$integer19,input$integer20,input$integer21, input$integer22,input$integer23,input$integer24)
     Data <- data.frame(Round, ScorePl3)
     
     
     
-    ggplot(Data, aes(x = Round, y = ScorePl3)) + geom_bar(stat = "identity", size = 2, fill = "red") + 
+    ggplot(Data, aes(x = Round, y = ScorePl3)) + geom_bar(stat = "identity", size = 2, fill = color) + 
       labs(x="Round", y="Score") +
-      scale_fill_hue(c=45, l=80) + #labs(title = "Player 3 Score") + 
-      ylim(-15, 80) + theme(panel.grid.major = element_blank(),
-                                                                                          panel.grid.minor = element_blank(),
-                                                                                          panel.border = element_blank(),
-                                                                                          panel.background = element_blank())
-    #ggtitle("Player 2") + 
-    #theme(plot.title = element_text(lineheight= 3.8, face="bold")) + theme(plot.title = element_text(size = rel(2)))
+      scale_fill_hue(c=45, l=80) + 
+      ylim(-15, 80) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), panel.background = element_blank())
+  
   })
 })
-
-
-
-
-
-# Run the application 
+ 
 shinyApp(ui = ui, server = server)
 
