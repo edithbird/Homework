06b1ef67e854c6328c49c0a51b30566fe36ee9fd
edithbird <edithbird5@gmail.com>
2017-03-04 21:49:56 -0500

    deleted extraneous code

--- ThisIsTheOne/app.R
+++ ThisIsTheOne/app.R
@@ -121,67 +121,7 @@ server <- function(input, output) ({
       
     )})
   
-  # output$barplot <- renderPlot({
-  #   #rainbowcols <- rainbow(8)
-  #   color <- c("slateblue3", "yellow", "red","slateblue3", "yellow", "red","slateblue3","red")
-  #   
-  #   Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
-  #   ScorePl1 <- c(input$integer1,input$integer3, input$integer5,input$integer7,input$integer9, input$integer11,input$integer13,input$integer15)
-  #   ScorePl2 <- c(input$integer2,input$integer4, input$integer6,input$integer8,input$integer10, input$integer12,input$integer14,input$integer16)
-  #   
-  #   Data <- data.frame(Round, ScorePl1, ScorePl2)
-  #   
-  #   
-  #   ggplot(Data, aes(x=Round, y=ScorePl1)) + geom_bar(stat = "identity",  size = 2, fill = color) + #"slateblue3"
-  #     labs(x="Round", y="Score") +
-  #     scale_fill_hue(c=45, l=80) +  #labs(title = "Player 1 Score") + 
-  #     ylim(-15, 80) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), panel.background = element_blank())
-  #     
-  #    }) 
-  # 
-  # output$barplot2 <- renderPlot({
-  #   color <- c("slateblue3", "yellow", "red","slateblue3", "yellow", "red","slateblue3","red")
-  #   rainbowcols <- rainbow(8)
-  #   Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
-  #   ScorePl1 <- c(input$integer1,input$integer3, input$integer5,input$integer7,input$integer9, input$integer11,input$integer13,input$integer15)
-  #   ScorePl2 <- c(input$integer2,input$integer4, input$integer6,input$integer8,input$integer10, input$integer12,input$integer14,input$integer16)
-  #   ScorePl3 <- c(input$integer17,input$integer18, input$integer19,input$integer20,input$integer21, input$integer22,input$integer23,input$integer24)
-  #   Data <- data.frame(Round, ScorePl1, ScorePl2, ScorePl3)
-  #   
-  #   
-  #   ggplot(Data, aes(x = Round, y = ScorePl2)) + geom_bar(stat = "identity", size = 2, fill = color) + #"yellow
-  #     labs(x="Round", y="Score") +
-  #     scale_fill_hue(c=45, l=80) +
-  #     ylim(-15, 80) + theme(panel.grid.major = element_blank(),
-  #   panel.grid.minor = element_blank(),
-  #   panel.border = element_blank(),
-  #   panel.background = element_blank())
-  # })
   
-  
-  # output$barplot3 <- renderPlot({
-  #   color <- c("slateblue3", "yellow", "red","slateblue3", "yellow", "red","slateblue3","red")
-  #   rainbowcols <- rainbow(8)
-  #   Round <- c("1", "2", "3", "4", "5", "6", "7", "8")
-  #   ScorePl3 <- c(input$integer17,input$integer18, input$integer19,input$integer20,input$integer21, input$integer22,input$integer23,input$integer24)
-  #   Data <- data.frame(Round, ScorePl3)
-  #   
-  #   
-  #   
-  #   ggplot(Data, aes(x = Round, y = ScorePl3)) + geom_bar(stat = "identity", size = 2, fill = color) + 
-  #     labs(x="Round", y="Score") +
-  #     scale_fill_hue(c=45, l=80) + 
-  #     ylim(-15, 80) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), panel.background = element_blank())
-  # 
-  # })
-  
-  # output$table2 <- renderTable({
-  #   data.frame(
-  #     Round_Number = c("1", "2", "3", "4", "5", "6", "7", "8", "1", "2", "3", "4", "5", "6", "7", "8", "1", "2", "3", "4", "5", "6", "7", "8"), 
-  #     Player = c("1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"), 
-  #     Score = c(input$integer1, input$integer2, input$integer17, input$integer3, input$integer4, input$integer18, input$integer5, input$integer6, input$integer19, input$integer7, input$integer8, input$integer20, input$integer9, input$integer10, input$integer21, input$integer11, input$integer12, input$integer22, input$integer13, input$integer14, input$integer23, input$integer15, input$integer16, input$integer24)
-  #    )
-  #   })
    output$barplot4 <- renderPlot({
      
      blue.bold.italic.16.text <- element_text(face = "bold", color = "black", size = 14)
