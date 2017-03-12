1f58b81788740d112207bd531d00d619dc8f8e4a
edithbird <edithbird5@gmail.com>
2017-03-12 10:56:53 -0400

    Imported CSS and got that style to work for the UI. Also, uploaded one of my files and changed the mtcars to the travel csv and was able to do histogram with the vehicle miles traveled.

--- PracticeButtons/app.R
+++ PracticeButtons/app.R
@@ -11,38 +11,58 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
+#this adds the bootstrap template  
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href =
-  "bootstrap.css")), 
+                "bootstrap.css")), 
   
   headerPanel("Geyser"),
-   
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  
+  sidebarPanel(
+    sliderInput("bins",
+                "Number of bins:",
+                min = 1,
+                max = 50,
+                value = 30)
+  ),
+  
+  navbarPage(
+    
+    # theme = "cerulean",  # <--- To use a theme, uncomment this
+    "shinythemes",
+    tabPanel("Navbar 1",
+             sidebarPanel(
+               fileInput("file", "File input:"),
+               textInput("txt", "Text input:", "general"),
+               sliderInput("slider", "Slider input:", 1, 100, 30),
+               tags$h5("Deafult actionButton:"),
+               actionButton("action", "Search"),
+               
+               tags$h5("actionButton with CSS class:"),
+               actionButton("action2", "Action button", class = "btn-primary")
+    
+    
+  ),
+  
+  
+  
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("distPlot")
+  ))))
 
-
+travel <- read.csv("data/Travel.csv")
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      VMT    <- travel[, 4] 
+      bins <- seq(min(VMT), max(VMT), length.out = input$bins + 1)
       
       # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      hist(VMT, breaks = bins, col = 'maroon', border = 'purple')
    })
 }
 
